5796416f574dcbacb42a25bb69059c85614bab85
anil <anil.ozder@microsay.com.tr>
2020-02-18 15:08:43 +0300

    hollow -1
    hollow -2

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -109,7 +109,7 @@ int main(void)
   /* USER CODE BEGIN 2 */
 HAL_TIM_Base_Start_IT(&htim10);
 uint8_t asd[]= "Hello World -8\r\n";
-uint8_t sss[]= "Hollow World \r\n";
+uint8_t sss[]= "Hollow World -1\r\n";
   /* USER CODE END 2 */
  
  
